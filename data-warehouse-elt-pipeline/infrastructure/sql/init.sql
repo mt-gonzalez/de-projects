84347ed0b0c58a49637a8bd3704f68e645749e86
Matias Gonzalez <matiastomasglez@gmail.com>
2026-03-23 16:43:37 -0300

    add table creation in sql init for a fixed schema ingestion

--- data-warehouse-elt-pipeline/infrastructure/sql/init.sql
+++ data-warehouse-elt-pipeline/infrastructure/sql/init.sql
@@ -1,3 +1,19 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
 CREATE SCHEMA IF NOT EXISTS raw;
+
+CREATE TABLE IF NOT EXISTS raw.events (
+    id UUID DEFAULT gen_random_uuid(),
+    event_time timestamp,
+    event_type text,
+    product_id bigint,
+    category_id bigint,
+    category_code text,
+    brand text,
+    price numeric(10,2),
+    user_id bigint,
+    user_session text
+);
+
 CREATE SCHEMA IF NOT EXISTS staging;
 CREATE SCHEMA IF NOT EXISTS marts;
