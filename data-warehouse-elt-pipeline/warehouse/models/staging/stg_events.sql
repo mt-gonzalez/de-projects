64553e83e983b0519b278128da2c89752de22d33
Matias Gonzalez <matiastomasglez@gmail.com>
2026-03-26 16:10:57 -0300

    updated stg_events view to be more consistent

--- data-warehouse-elt-pipeline/warehouse/models/staging/stg_events.sql
+++ data-warehouse-elt-pipeline/warehouse/models/staging/stg_events.sql
@@ -1,6 +1,6 @@
 SELECT 
-    CAST(event_time AS TIMESTAMP) AS event_time,
-    event_type,
+    CAST(event_time AS TIMESTAMP) AS event_timestamp,
+    LOWER(event_type),
     CAST(product_id AS BIGINT) AS product_id,
     CAST(category_id AS BIGINT) AS category_id,
     COALESCE(category_code, 'unknown') AS category_code,
