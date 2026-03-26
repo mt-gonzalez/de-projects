3e74aee3b4704a3f88e8a93a2687923bf9b7001f
Matias Gonzalez <matiastomasglez@gmail.com>
2026-03-26 18:50:42 -0300

    added deduplication to the fact events view query, due to duplicated rows

--- data-warehouse-elt-pipeline/warehouse/models/intermediate/int_fct_events.sql
+++ data-warehouse-elt-pipeline/warehouse/models/intermediate/int_fct_events.sql
@@ -1,5 +1,28 @@
 -- Events Fact Table; grain: 1 event = 1 row
 
+WITH deduplicate AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (
+            PARTITION BY 
+                user_id,
+                user_session,
+                event_timestamp,
+                event_type,
+                product_id
+            ORDER BY event_timestamp
+        ) AS row_number
+    FROM {{ ref('stg_events')}}
+),
+
+clean_stg_events AS (
+    SELECT *
+    FROM deduplicate
+    WHERE row_number = 1
+)
+
+-- It was found running the tests for this model that exists 629 duplicated rows, so first we apply deduplication to the view
+
 SELECT
     {{ dbt_utils.generate_surrogate_key([
         'user_id',
@@ -21,4 +44,4 @@ SELECT
     CASE WHEN event_type='purchase' THEN 1 ELSE 0 END AS is_purchase,
     price,
     CASE WHEN event_type='purchase' THEN price ELSE 0 END AS revenue
-FROM {{ ref('stg_events') }}
+FROM clean_stg_events
