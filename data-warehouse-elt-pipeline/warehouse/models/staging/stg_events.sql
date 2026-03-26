997d891f64700bbb1d7694ac235d7654f3c98c0d
Matias Gonzalez <matiastomasglez@gmail.com>
2026-03-26 17:04:31 -0300

    last update for stg_events model

--- data-warehouse-elt-pipeline/warehouse/models/staging/stg_events.sql
+++ data-warehouse-elt-pipeline/warehouse/models/staging/stg_events.sql
@@ -1,6 +1,6 @@
 SELECT 
     CAST(event_time AS TIMESTAMP) AS event_timestamp,
-    LOWER(event_type),
+    LOWER(event_type) AS event_type,
     CAST(product_id AS BIGINT) AS product_id,
     CAST(category_id AS BIGINT) AS category_id,
     COALESCE(category_code, 'unknown') AS category_code,
