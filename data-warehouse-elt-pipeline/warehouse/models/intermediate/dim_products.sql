3537e535ac28fc791414b206aa2e768aa5a731d6
Matias Gonzalez <matiastomasglez@gmail.com>
2026-03-27 17:21:27 -0300

    fix in dim products group by clause

--- data-warehouse-elt-pipeline/warehouse/models/intermediate/dim_products.sql
+++ data-warehouse-elt-pipeline/warehouse/models/intermediate/dim_products.sql
@@ -1,8 +1,9 @@
 SELECT
     product_id,
-    brand,
-    category_id,
-    category_code,
-    price
+    
+    -- We need this columns to be in a group by clause or on an aggregate function or postgresql will fail
+    MIN(brand) AS brand,
+    MIN(category_id) AS category_id,
+    MIN(category_code) AS category_code
 FROM {{ ref('int_events') }}
 GROUP BY product_id
