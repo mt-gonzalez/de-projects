d5e8738b6c5225eb60e69667854669fc0ce28a9a
Matias Gonzalez <matiastomasglez@gmail.com>
2026-03-27 14:32:33 -0300

    dim_dates modify using dbt_utils date_spine

--- data-warehouse-elt-pipeline/warehouse/models/intermediate/dim_date.sql
+++ data-warehouse-elt-pipeline/warehouse/models/intermediate/dim_date.sql
@@ -1,10 +1,20 @@
 -- Date dimension table
+{{ config(materialized='table') }}
 
-SELECT 
-    event_date,
-    EXTRACT(YEAR FROM event_date) AS date_year PRIMARY KEY,
-    EXTRACT(MONTH FROM event_date) AS date_month,
-    EXTRACT(QUARTER FROM event_date) AS date_quarter,
-    EXTRACT(WEEK FROM event_date) AS date_week,
-    EXTRACT(DAY FROM event_date) AS date_day
-FROM {{ ref('int_fct_events')}}
+WITH dates_raw AS (
+    {{ dbt_utils.date_spine(
+        datepart="day",
+        start_date="CAST('2020-09-24' AS DATE)",
+        end_date="CAST('2021-03-01' AS DATE)"
+    )
+    }}
+)
+SELECT
+    date_day AS date,
+    EXTRACT(YEAR FROM date_day) AS date_year,
+    EXTRACT(MONTH FROM date_day) AS date_month,
+    EXTRACT(QUARTER FROM date_day) AS date_quarter,
+    EXTRACT(WEEK FROM date_day) AS date_week,
+    EXTRACT(DAY FROM date_day) AS day_of_month,
+    CASE WHEN EXTRACT(DOW FROM date_day) IN (0,6) THEN true ELSE false END AS is_weekend
+FROM dates_raw
