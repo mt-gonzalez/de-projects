8ce3b108717773e741e0554a3777a34be7b1abac
Matias Gonzalez <matiastomasglez@gmail.com>
2026-03-26 23:55:40 -0300

    fix in duration column

--- data-warehouse-elt-pipeline/warehouse/models/intermediate/dim_sessions.sql
+++ data-warehouse-elt-pipeline/warehouse/models/intermediate/dim_sessions.sql
@@ -8,7 +8,7 @@ SELECT
     MAX(event_timestamp) AS session_end,
     MIN(event_date) AS session_start_date,
     MAX(event_date) AS session_end_date,
-    (MAX(session_start) - MIN(session_end)) AS duration
+    (MAX(event_timestamp) - MIN(event_timestamp)) AS duration
 FROM {{ ref('int_fct_events')}}
 GROUP BY session_id
 
